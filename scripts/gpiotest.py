18ec144d925f9d59444316031e071b0100951268
AkimotoHaruma <siba16fb@gmail.com>
2022-04-13 15:50:08 +0000

    test commit

--- scripts/gpiotest.py
+++ scripts/gpiotest.py
@@ -11,9 +11,10 @@ pi.set_mode(14, pigpio.OUTPUT)
 try:
     while True:
         pi.write(14,pigpio.HIGH)
-        print(pi.read(17))
+        print(pi.read(14))
         sleep(0.5)
-        pi.write(14, pigpio.LOW)
+        pi.write(14,pigpio.LOW)
+        print(pi.read(14))
         sleep(0.5)
 except KeyboardInterrupt:
     pass   
