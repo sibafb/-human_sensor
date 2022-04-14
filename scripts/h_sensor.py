e3481953da8fd716cca337003f059b067d42bd66
AkimotoHaruma <siba16fb@gmail.com>
2022-04-14 21:56:45 +0000

    add h sensor

--- scripts/h_sensor.py
+++ scripts/h_sensor.py
@@ -11,7 +11,7 @@ import pigpio
 HUMAN_SENSOR_SIGNAL = 18
 
 class HumanSensor:
-    def __init__(self, pin_asign = HUMAN_SENSOR_SIGNAL, IO_instance = pigpio.pi()):
+    def __init__(self, pin_asign = HUMAN_SENSOR_SIGNAL, IO_instance = pigpio.pi(), Topic_name = 'human_detect'):
 
         self.is_human_detect = False
 
@@ -24,20 +24,27 @@ class HumanSensor:
 
         self.sensor_value = self.IO.input(self.pin)
 
+        self.publisher = rospy.Publisher( Topic_name, Bool, queue_size=5)
+
     def cb_rising_edge(self):
         self.is_human_detect = True
+        self.publisher(self.is_human_detect)
 
     def cb_falling_edge(self):
         self.is_human_detect = False
+        self.publisher(self.is_human_detect)
 
     def is_detect(self):
-
         return self.s_human_detect 
 
 
 if __name__ == '__main__':
+
+    human_sensor = HumanSensor()
     try:
-        pass
+        while not rospy.is_shutdown():
+            #for now 
+            rate.sleep()
     except KeyboardInterrupt:
         pass
     finally:
